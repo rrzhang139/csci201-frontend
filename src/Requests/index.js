49af923d2f28636d6a1e09c6297e05999eaee3a5
Matthew Wilson <mtwilson@usc.edu>
2021-04-25 15:14:05 -0400

    request

--- src/Requests/index.js
+++ src/Requests/index.js
@@ -1,12 +1,12 @@
 export async function loginUser(credentials) {
-    return fetch(`http://localhost:8080/rrzhang_CSCI201L_Assignment4_/LoginServlet?username=${credentials.username}&password=${credentials.password}`, {
+    return fetch(`http://localhost:8080/LoginPage/LoginServlet?username=${credentials.username}&password=${credentials.password}`, {
         method: 'GET'
     })
         .then(data => data.json())
 }
 
 export async function registerUser(credentials) {
-    return await fetch(`http://localhost:8080/rrzhang_CSCI201L_Assignment4_/RegisterServlet?username=${credentials.username}&password=${credentials.password}&email=${credentials.email}`, {
+    return await fetch(`http://localhost:8080/LoginPage/RegisterServlet?username=${credentials.username}&password=${credentials.password}&email=${credentials.email}`, {
         method: 'POST'
     }).then(res => res.text())//.catch(() => alert("Either username or email is taken"))
 }
